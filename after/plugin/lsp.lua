261ce7c9d47ca887e92ff88ca8d5cecd90414921
powersindustries <31283557+powersindustries@users.noreply.github.com>
2024-11-05 10:20:33 -0600

    Update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,3 +1,12 @@
+-- LSP Setup.
+local lsp_zero = require('lsp-zero')
+
+-- Default setup.
+lsp_zero.on_attach(function(client, bufnr)
+    lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- Auto complete setup.
 local cmp = require "cmp"
 
 cmp.setup {
@@ -9,7 +18,29 @@ cmp.setup {
       ['<TAB>'] = cmp.mapping.confirm(
       {
           behavior = cmp.ConfirmBehavior.Insert,
-          select = true 
+          select = true
       }),
     }),
 }
+
+-- Require languages.
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+      'clangd',
+      'lua_ls'
+  },
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
+})
+
+
+-- In-editor error handling.
+vim.diagnostic.config({
+    virtual_text = false,
+    virtual_lines = false,
+    signs = false,
+})
